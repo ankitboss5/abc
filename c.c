e4ebe2c6f769a1b27f7d7a833f55d70b9bf3bd29
ankitboss5 <111964304+ankitboss5@users.noreply.github.com>
2023-02-23 17:06:13 +0530

    Update c.c

--- c.c
+++ c.c
@@ -1,15 +1,7 @@
 #include<stdio.h>
 int main(){
-
-    int a,b;
-    printf("a is");
-    scanf("%d",& a);
-    printf("a is%d",a);
-     printf("b is");
-    scanf("%d",&b);
-    printf("b is%d",b);
-   int a==b;
-   int b==a;
+printf("hello word");
+ 
 
     return 0;
 }
