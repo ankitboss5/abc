226f0884cdc85ad497521e40335cdadf30da0372
ankitboss5 <111964304+ankitboss5@users.noreply.github.com>
2023-02-23 03:11:53 -0800

    Add files via upload

---
+++ c.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int a,b;
+    printf("a is");
+    scanf("%d",& a);
+    printf("a is%d",a);
+     printf("b is");
+    scanf("%d",&b);
+    printf("b is%d",b);
+   int a==b;
+   int b==a;
+
+    return 0;
+}
